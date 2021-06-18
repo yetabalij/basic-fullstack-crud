02f3466eda73dd515f57e526be91d13fb14b41ac
yetabalij <ermiastsegu@yahoo.com>
2021-06-18 22:20:30 +0300

    car route middleware is inserted to server.js file.

--- backend/server.js
+++ backend/server.js
@@ -5,23 +5,29 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const carRoute = require("./routers/car.router");
+
 //creating express app
 const app = express();
 
 //db connection
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-}).then(()=>{
-    console.log("Db is connected.")
-});
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Db is connected.");
+  });
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
 //creating routing middleware
+app.use("/api", carRoute);
 
 //creating a port for the app
 const port = process.env.PORT || 5000;
