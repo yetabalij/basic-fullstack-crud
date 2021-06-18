11182cf9fa4949fdd47f6a3ac7a00873179f3c55
yetabalij <ermiastsegu@yahoo.com>
2021-06-18 21:21:18 +0300

    server.js file is created and db connected and server up and run.

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+//importing required moduels
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//creating express app
+const app = express();
+
+//db connection
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Db is connected.")
+});
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+//creating routing middleware
+
+//creating a port for the app
+const port = process.env.PORT || 5000;
+
+//let app listen to the specified port.
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
