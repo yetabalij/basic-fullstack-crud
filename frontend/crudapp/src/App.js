b25ee1586d80e67a8e502f8efcc98897e7b68c98
yetabalij <ermiastsegu@yahoo.com>
2021-06-19 23:16:22 +0300

    car data is fetched using axios and placed in the app component the passed as a prop to crudtable basictable compnent.

--- frontend/crudapp/src/App.js
+++ frontend/crudapp/src/App.js
@@ -1,11 +1,21 @@
 import BasicTable from "./components/crudtable";
-function App() {
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [Cardata, setData] = useState([]);
+  const uri = "http://localhost:5000/api/getAllCars";
+  useEffect(() => {
+    axios.get(uri).then((response) => {
+      setData(response.data);
+    });
+  }, []);
   return (
     <div>
       <h3>Car Info.</h3>
-      <BasicTable/>
+      <BasicTable cardata={Cardata} />
     </div>
   );
-}
+};
 
 export default App;
