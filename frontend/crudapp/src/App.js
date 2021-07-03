9fd2f56a0f009d73bb11aede165e5ffaa20388dc
yetabalij <ermiastsegu@yahoo.com>
2021-07-03 22:22:50 +0300

    routing is completed.

--- frontend/crudapp/src/App.js
+++ frontend/crudapp/src/App.js
@@ -1,12 +1,67 @@
+import React from "react";
+import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 
+//material-ui imports
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    color: "white",
+    textDecoration: "none",
+    marginLeft: 20,
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
   return (
     <div>
-      <h3>Car Info.</h3>
-      <Home />
-      <Register />
+      <BrowserRouter>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Car Info!
+              </Typography>
+              <Link to="/" className={classes.link}>
+                Home
+              </Link>
+              <Link to="/register" className={classes.link}>
+                Register
+              </Link>
+            </Toolbar>
+          </AppBar>
+        </div>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
