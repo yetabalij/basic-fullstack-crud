c3d0b03746c4dd689a29796448991648d49bcf1e
yetabalij <ermiastsegu@yahoo.com>
2021-07-03 21:17:55 +0300

    Home and Register page are imported. State is moved to Home component

--- frontend/crudapp/src/App.js
+++ frontend/crudapp/src/App.js
@@ -1,19 +1,12 @@
-import BasicTable from "./components/crudtable";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 const App = () => {
-  const [Cardata, setData] = useState([]);
-  const uri = "http://localhost:5000/api/getAllCars";
-  useEffect(() => {
-    axios.get(uri).then((response) => {
-      setData(response.data);
-    });
-  }, []);
   return (
     <div>
       <h3>Car Info.</h3>
-      <BasicTable cardata={Cardata} />
+      <Home />
+      <Register />
     </div>
   );
 };
